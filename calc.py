1d66649e854690fe3dde7c4f654f9334b336b5dc
Hamdini Cherif <ryadhamdini@gmail.com>
2023-05-15 19:23:41 +0200

    Dockerfile added

--- calc.py
+++ calc.py
@@ -1,6 +1,73 @@
 import math
 
 class Calculator:
+    def display_menu(self):
+        print("Calculator Menu:")
+        print("1. Add")
+        print("2. Subtract")
+        print("3. Multiply")
+        print("4. Divide")
+        print("5. Power")
+        print("6. Square Root")
+        print("7. Sine")
+        print("8. Cosine")
+        print("9. Tangent")
+        print("10. Logarithm")
+        print("0. Exit")
+
+    def run(self):
+        while True:
+            self.display_menu()
+            choice = input("Enter your choice: ")
+
+            if choice == "0":
+                print("Exiting...")
+                break
+
+            try:
+                if choice == "1":
+                    a = float(input("Enter the first number: "))
+                    b = float(input("Enter the second number: "))
+                    print("Result:", self.add(a, b))
+                elif choice == "2":
+                    a = float(input("Enter the first number: "))
+                    b = float(input("Enter the second number: "))
+                    print("Result:", self.subtract(a, b))
+                elif choice == "3":
+                    a = float(input("Enter the first number: "))
+                    b = float(input("Enter the second number: "))
+                    print("Result:", self.multiply(a, b))
+                elif choice == "4":
+                    a = float(input("Enter the numerator: "))
+                    b = float(input("Enter the denominator: "))
+                    print("Result:", self.divide(a, b))
+                elif choice == "5":
+                    a = float(input("Enter the base: "))
+                    b = float(input("Enter the exponent: "))
+                    print("Result:", self.power(a, b))
+                elif choice == "6":
+                    a = float(input("Enter the number: "))
+                    print("Result:", self.square_root(a))
+                elif choice == "7":
+                    a = float(input("Enter the angle in radians: "))
+                    print("Result:", self.sin(a))
+                elif choice == "8":
+                    a = float(input("Enter the angle in radians: "))
+                    print("Result:", self.cos(a))
+                elif choice == "9":
+                    a = float(input("Enter the angle in radians: "))
+                    print("Result:", self.tan(a))
+                elif choice == "10":
+                    a = float(input("Enter the number: "))
+                    print("Result:", self.log(a))
+                else:
+                    print("Invalid choice. Please try again.")
+            except ValueError as e:
+                print("Error:", str(e))
+            except ZeroDivisionError:
+                print("Error: Cannot divide by zero")
+
+                
     def add(self, a, b):
         return a + b
 
@@ -39,11 +106,6 @@ class Calculator:
 
 if __name__ == "__main__":
     calculator = Calculator()
-
-    # Example usage
-    print(calculator.add(2, 3))
-    print(calculator.multiply(4, 5))
-    print(calculator.divide(10, 2))
-    print(calculator.square_root(16))
+    calculator.run()
 
 
