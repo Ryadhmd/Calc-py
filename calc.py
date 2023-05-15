9b7cc28e52b9f29a497879a19fb73518d5d9cd90
Hamdini Cherif <ryadhamdini@gmail.com>
2023-05-15 17:50:45 +0200

    moddified calc.py

--- calc.py
+++ calc.py
@@ -1,38 +1,49 @@
 import math
 
-def add(a, b):
-    return a + b
+class Calculator:
+    def add(self, a, b):
+        return a + b
 
-def subtract(a, b):
-    return a - b
+    def subtract(self, a, b):
+        return a - b
 
-def multiply(a, b):
-    return a * b
+    def multiply(self, a, b):
+        return a * b
 
-def divide(a, b):
-    if b == 0:
-        raise ValueError("Cannot divide by zero")
-    return a / b
+    def divide(self, a, b):
+        if b == 0:
+            raise ValueError("Cannot divide by zero")
+        return a / b
 
-def power(a, b):
-    return math.pow(a, b)
+    def power(self, a, b):
+        return math.pow(a, b)
 
-def square_root(a):
-    if a < 0:
-        raise ValueError("Cannot take the square root of a negative number")
-    return math.sqrt(a)
+    def square_root(self, a):
+        if a < 0:
+            raise ValueError("Cannot take the square root of a negative number")
+        return math.sqrt(a)
 
-def sin(a):
-    return math.sin(a)
+    def sin(self, a):
+        return math.sin(a)
 
-def cos(a):
-    return math.cos(a)
+    def cos(self, a):
+        return math.cos(a)
 
-def tan(a):
-    return math.tan(a)
+    def tan(self, a):
+        return math.tan(a)
 
-def log(a):
-    if a <= 0:
-        raise ValueError("Cannot take the logarithm of a non-positive number")
-    return math.log(a)
+    def log(self, a):
+        if a <= 0:
+            raise ValueError("Cannot take the logarithm of a non-positive number")
+        return math.log(a)
+
+if __name__ == "__main__":
+    calculator = Calculator()
+
+    # Example usage
+    print(calculator.add(2, 3))
+    print(calculator.multiply(4, 5))
+    print(calculator.divide(10, 2))
+    print(calculator.square_root(16))
+
 
--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,51 +1,55 @@
 import pytest
-from calc import *
+import math
+from calc import Calculator
+
+calculator = Calculator()
 
 def test_add():
-    assert add(2, 3) == 5
-    assert add(0, 0) == 0
-    assert add(-1, 1) == 0
+    assert calculator.add(2, 3) == 5
+    assert calculator.add(0, 0) == 0
+    assert calculator.add(-1, 1) == 0
 
 def test_subtract():
-    assert subtract(2, 3) == -1
-    assert subtract(0, 0) == 0
-    assert subtract(-1, 1) == -2
+    assert calculator.subtract(2, 3) == -1
+    assert calculator.subtract(0, 0) == 0
+    assert calculator.subtract(-1, 1) == -2
 
 def test_multiply():
-    assert multiply(2, 3) == 6
-    assert multiply(0, 0) == 0
-    assert multiply(-1, 1) == -1
+    assert calculator.multiply(2, 3) == 6
+    assert calculator.multiply(0, 0) == 0
+    assert calculator.multiply(-1, 1) == -1
 
 def test_divide():
-    assert divide(6, 3) == 2
-    assert divide(0, 5) == 0
-    assert divide(-1, 1) == -1
+    assert calculator.divide(6, 3) == 2
+    assert calculator.divide(0, 5) == 0
+    assert calculator.divide(-1, 1) == -1
     with pytest.raises(ValueError):
-        divide(5, 0)
+        calculator.divide(5, 0)
 
 def test_power():
-    assert power(2, 3) == 8
-    assert power(0, 0) == 1
-    assert power(-1, 2) == 1
+    assert calculator.power(2, 3) == 8
+    assert calculator.power(0, 0) == 1
+    assert calculator.power(-1, 2) == 1
 
 def test_square_root():
-    assert square_root(9) == 3
-    assert square_root(0) == 0
+    assert calculator.square_root(9) == 3
+    assert calculator.square_root(0) == 0
     with pytest.raises(ValueError):
-        square_root(-1)
+        calculator.square_root(-1)
 
 def test_sin():
-    assert math.isclose(sin(math.pi/2), 1, rel_tol=1e-9)
+    assert math.isclose(calculator.sin(math.pi/2), 1, rel_tol=1e-9)
 
 def test_cos():
-    assert math.isclose(cos(0), 1, rel_tol=1e-9)
+    assert math.isclose(calculator.cos(0), 1, rel_tol=1e-9)
 
 def test_tan():
-    assert math.isclose(tan(math.pi/4), 1, rel_tol=1e-9)
+    assert math.isclose(calculator.tan(math.pi/4), 1, rel_tol=1e-9)
 
 def test_log():
-    assert math.isclose(log(math.e), 1, rel_tol=1e-9)
+    assert math.isclose(calculator.log(math.e), 1, rel_tol=1e-9)
     with pytest.raises(ValueError):
-        log(0)
-        log(-1)
+        calculator.log(0)
+        calculator.log(-1)
+
 
